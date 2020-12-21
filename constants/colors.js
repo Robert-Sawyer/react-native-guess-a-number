8f9eaa7a2df7b3c7fa4b101bbc136bf0c1fa697c
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-12-21 13:55:05 +0100

    Imported common colors to the styles in components

---
+++ constants/colors.js
@@ -0,0 +1,5 @@
+export default {
+    headerColor: '#f7287b',
+    confirmColor: '#39a324',
+    cancelOrReset: '#d40202',
+}
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {View, Text, StyleSheet, TextInput, Button} from 'react-native'
 import Card from "../components/Card";
+import Colors from '../constants/colors'
 
 const StartGameScreen = props => {
     return (
@@ -11,8 +12,12 @@ const StartGameScreen = props => {
                 <Text>Select a number</Text>
                 <TextInput/>
                 <View style={styles.buttonContainer}>
-                    <Button title='Reset' onPress={() => {}}/>
-                    <Button title='Confirm' onPress={() => {}}/>
+                    <View style={styles.button}>
+                        <Button title='Reset' color={Colors.cancelOrReset} onPress={() => {}}/>
+                    </View>
+                    <View style={styles.button}>
+                    <Button title='Confirm' color={Colors.confirmColor} onPress={() => {}}/>
+                    </View>
                 </View>
             </Card>
         </View>
@@ -39,6 +44,9 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'space-between',
         paddingHorizontal: 15,
+    },
+    button: {
+      width: '40%',
     },
 })
 
