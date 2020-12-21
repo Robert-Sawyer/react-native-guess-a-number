f29dc6d1401262019074324c477aa171621f5d38
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-12-21 13:18:58 +0100

    Added the first screen view

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native'
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>Start the game!</Text>
+            <Text style={styles.title}>Start the game!</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a number</Text>
+                <TextInput/>
+                <View style={styles.buttonContainer}>
+                    <Button title='Reset' onPress={() => {
+                    }}/>
+                    <Button title='Confirm' onPress={() => {
+                    }}/>
+                </View>
+            </View>
         </View>
     )
 }
@@ -14,6 +24,33 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        //właściwości shadow... są dostępne tylko na iOS a elevation tylko na Androidzie - oba dotyczą stylowania cienia
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 5,
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
     },
 })
 
