d69cb78c7f5e2ba2233a32ac8fc2fe2f98222535
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-12-21 15:21:16 +0100

    Added validation to input on first screen + config Keyboard

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,37 +1,60 @@
-import React from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard} from 'react-native'
 import Card from "../components/Card";
 import Colors from '../constants/colors'
 import Input from "../components/Input";
 
 const StartGameScreen = props => {
+
+    //tworzę obsługę walidacji dla wprowadzanej wartości na androidzie
+    const [enteredValue, setEnteredValue] = useState('')
+
+    //metoda do walidacji wprowadzanych danych - wpisaną przez usera wartość zamieniam dzięki .replace():
+    //jeśli user wprowadził do inputa cokolwiek innego niż cyfry od 0 do 9, np, przecinek, lub kropkę, to zamień tę
+    //wartość na pusty string ''. 'g' oznacza w regexie flagę globalną - program obejmie całą aktywną cześc aplikacji
+    const handleNumberValue = (inputValue) => {
+        setEnteredValue(inputValue.replace(/[^0-9]/g, ''))
+    }
+
+    //w momencie kliknięcia na inputa pojawia sie klawiatura, ta metoda zamyka ją gdy kliknę obok
+    //Keyboard to nie komponent tylko interfejs API który dostarcza React Native by kontrolować klawiaturę
+    const handleCloseKeyboard = () => {
+        Keyboard.dismiss()
+    }
+
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start the game!</Text>
-            {/*Wysyłam props style do wewnątrz komponentu Card i tam merguję go z wewnętrznymi stylami Card*/}
-            <Card style={styles.inputContainer}>
-                <Text>Select a number</Text>
-                {/*TextInput, któy jest wewnątrz tego inputa ma wiele opcji konfiguracji i modyfikacji wprowadzanych
+        <TouchableWithoutFeedback onPress={handleCloseKeyboard}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start the game!</Text>
+                {/*Wysyłam props style do wewnątrz komponentu Card i tam merguję go z wewnętrznymi stylami Card*/}
+                <Card style={styles.inputContainer}>
+                    <Text>Select a number</Text>
+                    {/*TextInput, któy jest wewnątrz tego inputa ma wiele opcji konfiguracji i modyfikacji wprowadzanych
                 danych - wszystko w dokumentacji. Poniżej wykorzystam tylko rodzaj klawiatury, wyłączenie autokorekty,
                 rozmycie po kliknięciu i ograniczenie liczby do 99*/}
-                <Input
-                    style={styles.input}
-                    blurOnSubmit
-                    keyboardType='number-pad'
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    maxLength={2}
-                />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}>
-                        <Button title='Reset' color={Colors.cancelOrReset} onPress={() => {}}/>
+                    <Input
+                        style={styles.input}
+                        onChangeText={handleNumberValue}
+                        value={enteredValue}
+                        blurOnSubmit
+                        keyboardType='number-pad'
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        maxLength={2}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button title='Reset' color={Colors.cancelOrReset} onPress={() => {
+                            }}/>
+                        </View>
+                        <View style={styles.button}>
+                            <Button title='Confirm' color={Colors.confirmColor} onPress={() => {
+                            }}/>
+                        </View>
                     </View>
-                    <View style={styles.button}>
-                    <Button title='Confirm' color={Colors.confirmColor} onPress={() => {}}/>
-                    </View>
-                </View>
-            </Card>
-        </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
@@ -61,7 +84,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15,
     },
     button: {
-      width: '40%',
+        width: '40%',
     },
 })
 
