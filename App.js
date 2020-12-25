6f0e85ed6712b1e26004bcf04b7662df41d245ee
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-12-25 22:39:16 +0100

    Added final logic on the third screen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
@@ -9,10 +9,16 @@ export default function App() {
     const [userNumber, setUserNumber] = useState()
     const [guessRounds, setGuessRounds] = useState(0)
 
-    const handleStartGame = (selectedNumber) => {
-        setUserNumber(selectedNumber)
+    const handleNewGame = () => {
         //rozpoczynając grę ustawiam z powrotem liczbe zgadywań komputerea na 0
         setGuessRounds(0)
+        //ustawiam też liczbę usera na null, żeby poniżej, w content nie był spełniony żaden if i żeby uruchomił
+        //się pierwszy ekran
+        setUserNumber(null)
+    }
+
+    const handleStartGame = (selectedNumber) => {
+        setUserNumber(selectedNumber)
     }
 
     //chcę, żeby program liczył liczbę rund/zgadywańi gdy skończy, pokazywałza którym razem zgadł
@@ -24,22 +30,27 @@ export default function App() {
 
     //jeśli liczba rund jest zerowa to znaczy że gra się nie zaczęłą, a jeśli większa to znaczy, że należy wyświetlić
     //ekran końcowy
-    if(userNumber && guessRounds <= 0) {
+    if (userNumber && guessRounds <= 0) {
         content = <GameScreen userChoice={userNumber} onGameOver={handleGameOver}/>
     } else if (guessRounds > 0) {
-        content = <GameOverScreen/>
+        content =
+            <GameOverScreen
+                roundsNumber={guessRounds}
+                userNumber={userNumber}
+                onRestart={handleNewGame}
+            />
     }
 
-  return (
-    <View style={styles.screen}>
-      <Header title='Guess a Number'/>
-        {content}
-    </View>
-  );
+    return (
+        <View style={styles.screen}>
+            <Header title='Zgadnij liczbę'/>
+            {content}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
+    screen: {
+        flex: 1,
+    },
 });
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is Over!</Text>
+            <Text>Gra skończona!</Text>
+            <Text>Liczba rund: {props.roundsNumber}</Text>
+            <Text>Twoją liczbą było: {props.userNumber}</Text>
+            <Button title="NOWA GRA" onPress={props.onRestart}/>
         </View>
     )
 }
