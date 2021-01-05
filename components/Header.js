45a51d5fe38d2851da4cecb740eecefe2443d741
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-05 15:46:30 +0100

    Adjusted some styles depending on the OS and version

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,39 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Platform } from 'react-native'
 import Colors from '../constants/colors'
 import DefaultStyles from '../constants/default-styles'
 
 
 const Header = props => {
     return (
-        <View style={styles.header}>
+        <View style={{
+            ...styles.headerBase,
+            ...Platform.select({
+                ios: styles.headerIOS,
+                android: styles.headerAndroid
+            })}}>
             <Text style={DefaultStyles.headerTitle}>{props.title}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: Colors.headerColor,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    //w taki sposób można bardzo łatwo ustawiac przeróżne właściwości elementów aplikacji w zależności od systemu
+    //operacyjnego. dzięki temu rozdzieleniu kod jest czystszy i merguję go w style={} powyżej, w JSX
+    headerAndroid:{
+        backgroundColor: Colors.headerColor,
+    },
+    headerIOS: {
+        backgroundColor: '#db20bc',
+        borderBottomWidth: 2,
+        borderColor: '#000'
     },
 })
 
--- components/MyButton.js
+++ components/MyButton.js
@@ -1,18 +1,31 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform} from 'react-native'
 import Color from '../constants/colors'
 
 const MyButton = props => {
+
+    let ButtonComponent = TouchableOpacity
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        ButtonComponent = TouchableNativeFeedback
+    }
+
     return (
-        <TouchableOpacity onPress={props.onPress}>
-            <View style={{...styles.button, ...props.style}}>
-                <Text style={styles.buttonText}>{props.children}</Text>
-            </View>
-        </TouchableOpacity>
+        <View style={styles.buttonContainer}>
+            <ButtonComponent onPress={props.onPress}>
+                <View style={{...styles.button, ...props.style}}>
+                    <Text style={styles.buttonText}>{props.children}</Text>
+                </View>
+            </ButtonComponent>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
+    buttonContainer: {
+        borderRadius: 25,
+        overflow: 'hidden',
+    },
     button: {
         backgroundColor: Color.headerColor,
         paddingVertical: 12,
